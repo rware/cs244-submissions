e7bdd0d7d467d31a2ba504086482b933d2c89420
Vivek Jain <vivek@vivekja.in>
2016-04-17 16:48:57 -0700

    Change window size based on RTT

--- datagrump/controller.cc
+++ datagrump/controller.cc
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <cmath>
 
 #include "controller.hh"
 #include "timestamp.hh"
 
 using namespace std;
+
+static const uint64_t INCREASE_THRESHOLD = 90;
+static const uint64_t DECREASE_THRESHOLD = 150;
 
 /* Default constructor */
 Controller::Controller( const bool debug )
@@ -16,12 +20,12 @@ Controller::Controller( const bool debug )
 /* Get current window size, in datagrams */
 unsigned int Controller::window_size( void )
 {
-  if ( debug_ ) {
-    cerr << "At time " << timestamp_ms()
-   << " window size is " << window_size_int << endl;
-  }
+  // if ( debug_ ) {
+  //   cerr << "At time " << timestamp_ms()
+  //  << " window size is " << window_size_int << endl;
+  // }
 
-  return window_size_int;
+  return window_size_double;
 }
 
 /* A datagram was sent */
@@ -32,10 +36,10 @@ void Controller::datagram_was_sent( const uint64_t sequence_number,
 {
   /* Default: take no action */
 
-  if ( debug_ ) {
-    cerr << "At time " << send_timestamp
-   << " sent datagram " << sequence_number << endl;
-  }
+  // if ( debug_ ) {
+  //   cerr << "At time " << send_timestamp
+  //  << " sent datagram " << sequence_number << endl;
+  // }
 }
 
 /* An ack was received */
@@ -48,43 +52,37 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
              const uint64_t timestamp_ack_received )
                                /* when the ack was received (by sender) */
 {
-  if (timestamp_ack_received - send_timestamp_acked >= timeout_ms()) {
-    if (!in_timeout_batch) {
-      in_timeout_batch = true;
-      // Multiplicative decrease
-      window_size_double *= 0.5;
-      window_size_int = static_cast<unsigned int>(window_size_double);
-      if (window_size_int == 0) {
-        window_size_int = 1;
-      }
-      if (debug_) {
-        cerr << "timeout: window size is now " << window_size_int << endl;
-      }
+  if (timestamp_ack_received - send_timestamp_acked >= DECREASE_THRESHOLD) {
+    if (debug_) {
+        cerr << "RTT " << timestamp_ack_received - send_timestamp_acked << ", decreasing window size " << window_size_double << endl;
     }
+    if (window_size_double >= 1) {
+        window_size_double -= 1;
+    }
+  } else if (timestamp_ack_received - send_timestamp_acked <= INCREASE_THRESHOLD) {
+    if (debug_) {
+        cerr << "RTT " << timestamp_ack_received - send_timestamp_acked << ", increasing window size " << window_size_double << endl;
+    }
+    window_size_double += 1;
   } else {
-    in_timeout_batch = false;
-    // Additive increase
-    window_size_double += 1 / static_cast<double>(window_size_int);
-    if (static_cast<unsigned int>(window_size_double) > window_size_int) {
-      window_size_int = static_cast<unsigned int>(window_size_double);
-      if (debug_) {
-        cerr << "window size increased to " << window_size_int << endl;
-      }
+    if (debug_) {
+        cerr << "RTT " << timestamp_ack_received - send_timestamp_acked << ", doing nothing" << endl;
     }
   }
 
-  if ( debug_ ) {
-    cerr << "At time " << timestamp_ack_received
-   << " received ack for datagram " << sequence_number_acked
-   << " (send @ time " << send_timestamp_acked
-   << ", received @ time " << recv_timestamp_acked << " by receiver's clock)"
-   << endl;
-  }
+  // if ( debug_ ) {
+  //   cerr << "At time " << timestamp_ack_received
+  //  << " received ack for datagram " << sequence_number_acked
+  //  << " original packet sent " << send_timestamp
+  //  << " (send @ time " << send_timestamp_acked
+  //  << ", received @ time " << recv_timestamp_acked << " by receiver's clock)"
+  //  << endl;
+  // }
 }
 
 /* How long to wait (in milliseconds) if there are no acks
    before sending one more datagram */
 unsigned int Controller::timeout_ms( void )
 {
-  return 200;
+  return 500;
 }
