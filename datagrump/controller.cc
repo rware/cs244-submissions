145f157bcc9e8b3fccde708391b34ae7170b11ad
Andrew Fang <andrewbrianfang@gmail.com>
2016-04-18 20:58:26 -0700

    Scaffolding

--- datagrump/controller.cc
+++ datagrump/controller.cc
@@ -6,15 +6,15 @@
 using namespace std;
 
 /* Default constructor */
-Controller::Controller( const bool debug )
-  : debug_( debug )
+Controller::Controller( const bool debug)
+  : debug_( debug ), windowSize( 20 ), timeout ( 1000 )
 {}
 
 /* Get current window size, in datagrams */
 unsigned int Controller::window_size( void )
 {
   /* Default: fixed window size of 100 outstanding datagrams */
-  unsigned int the_window_size = 50;
+  unsigned int the_window_size = this->windowSize;
 
   if ( debug_ ) {
     cerr << "At time " << timestamp_ms()
@@ -25,10 +25,8 @@ unsigned int Controller::window_size( void )
 }
 
 /* A datagram was sent */
-void Controller::datagram_was_sent( const uint64_t sequence_number,
-				    /* of the sent datagram */
-				    const uint64_t send_timestamp )
-                                    /* in milliseconds */
+void Controller::datagram_was_sent( const uint64_t sequence_number, /* of the sent datagram */
+				    const uint64_t send_timestamp ) /* in milliseconds */
 {
   /* Default: take no action */
 
@@ -39,16 +37,18 @@ void Controller::datagram_was_sent( const uint64_t sequence_number,
 }
 
 /* An ack was received */
-void Controller::ack_received( const uint64_t sequence_number_acked,
-			       /* what sequence number was acknowledged */
-			       const uint64_t send_timestamp_acked,
-			       /* when the acknowledged datagram was sent (sender's clock) */
-			       const uint64_t recv_timestamp_acked,
-			       /* when the acknowledged datagram was received (receiver's clock)*/
-			       const uint64_t timestamp_ack_received )
-                               /* when the ack was received (by sender) */
+void Controller::ack_received( const uint64_t sequence_number_acked, /* what sequence number was acknowledged */
+			       const uint64_t send_timestamp_acked, /* when the acknowledged datagram was sent (sender's clock) */
+			       const uint64_t recv_timestamp_acked, /* when the acknowledged datagram was received (receiver's clock)*/
+			       const uint64_t timestamp_ack_received ) /* when the ack was received (by sender) */
 {
   /* Default: take no action */
+
+  if (packetSuccessfullyAcked) {
+    this->windowSize++;
+  } else if (timeoutComputed > this.timeout_ms()) {
+    this->windowSize = this->windowSize / 2;
+  }
 
   if ( debug_ ) {
     cerr << "At time " << timestamp_ack_received
@@ -63,5 +63,5 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
    before sending one more datagram */
 unsigned int Controller::timeout_ms( void )
 {
-  return 1000; /* timeout of one second */
+  return this->timeout; /* timeout of one second */
 }
