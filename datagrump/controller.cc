9005cabe9ab89d31034cdfdae65973f60110ca33
Jintian Liang <jtliang@DN51ro0s.SUNet>
2016-04-22 00:37:50 -0700

    Removed unnecessary functions in controller.cc for delay-based congestion control

--- datagrump/controller.cc
+++ datagrump/controller.cc
@@ -39,39 +39,7 @@ void Controller::datagram_was_sent( const uint64_t sequence_number,
   }
 }
 
-uint64_t diff_ms(timeval t1, timeval t2)
-{
-    return (uint64_t)(((t1.tv_sec - t2.tv_sec) * 1000000) + 
-            (t1.tv_usec - t2.tv_usec))/1000;
-}
-
-void Controller::delay_aiad_unsmoothedRTT(const uint64_t sequence_number_acked,
-			       const uint64_t send_timestamp_acked,
-			       const uint64_t timestamp_ack_received ) {
-
-  uint64_t newRoundTripTime = timestamp_ack_received - send_timestamp_acked;
-  num_packets_received++;
-  rtt_total += newRoundTripTime;
-  bool firstPacket = num_packets_received == 1;
-  if (firstPacket) {
-    rtt_estimate = newRoundTripTime;
-  } else {
-    if (newRoundTripTime <= rtt_estimate) {
-    	the_window_size++;
-    } else if (newRoundTripTime > rtt_estimate) {
-    	the_window_size--;
-      if (the_window_size <= 1) {
-        the_window_size = 1;
-      }
-    }
-	rtt_estimate = rtt_total / (float)num_packets_received;	
-  }
-  if ( debug_ ) {
-    cerr << endl << "The estimated rtt for datagram " << sequence_number_acked << " is " << newRoundTripTime << ". " << endl << "The new rtt estimate is " << rtt_estimate << "." << endl << endl;
-  }
-}
-
-void Controller::delay_aimd_smoothedRTT( const uint64_t sequence_number_acked,
+void Controller::delay_aiad_unsmoothedRTT( const uint64_t sequence_number_acked,
              const uint64_t send_timestamp_acked,
              const uint64_t timestamp_ack_received )
 {
@@ -108,7 +76,7 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
                                /* when the ack was received (by sender) */
 {
   /* Default: take no action */
-  delay_aimd_smoothedRTT(sequence_number_acked, send_timestamp_acked, timestamp_ack_received);
+  delay_aiad_unsmoothedRTT(sequence_number_acked, send_timestamp_acked, timestamp_ack_received);
   
 
   if ( debug_ ) {
