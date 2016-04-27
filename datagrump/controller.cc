b30c8ff4e5db57c95938353f552487ab8567f155
Eli Berg <ejberg@stanford.edu>
2016-04-27 16:45:38 -0700

    add throughput estimates

--- datagrump/controller.cc
+++ datagrump/controller.cc
@@ -9,18 +9,29 @@
 #define RTT_THRESH 125
 #define SLOW_ST_THRESH 15
 
+#define TIME_SLICE_LEN 50
+#define THROUGHPUT_GUESS ((double)15 / (double)100)
+#define THROUGHPUT_EST_WEIGHT 0.3
+
 using namespace std;
 
 /* Default constructor */
 Controller::Controller( const bool debug )
-  : debug_( debug ), cwnd(CWND_DEFAULT), rtt_avg(RTT_GUESS), 
-    slow_st_thresh(SLOW_ST_THRESH)
-{}
+  : debug_( debug ), cwnd(CWND_DEFAULT), slow_st_thresh(SLOW_ST_THRESH),
+    acks_received(0), rtts(), index(0), time_slice_start(0),
+    est_throughput(THROUGHPUT_GUESS)
+
+{
+  for (int i = 0; i < NUM_RTTS; i++) {
+    rtts[i] = RTT_GUESS;
+  }
+}
 
 /* Get current window size, in datagrams */
 unsigned int Controller::window_size( void )
 {
   /* Default: fixed window size of 100 outstanding datagrams */
+  cwnd = rtt_avg() * est_throughput;
 
   if ( debug_ ) {
     cerr << "At time " << timestamp_ms()
@@ -30,6 +41,32 @@ unsigned int Controller::window_size( void )
   return cwnd;
 }
 
+void Controller::update(uint64_t cur_time) {
+    if (cur_time > time_slice_start + TIME_SLICE_LEN) {
+        double new_throughput = (double) acks_received / TIME_SLICE_LEN;
+        est_throughput = new_throughput * THROUGHPUT_EST_WEIGHT 
+            + (1.0 - THROUGHPUT_EST_WEIGHT) * est_throughput;
+        
+        time_slice_start += TIME_SLICE_LEN;
+        acks_received = 0;
+    }
+}
+
+void Controller::rtt_add(unsigned int rtt) {
+    rtts[index] = rtt;
+    index++;
+    if (index == NUM_RTTS) index = 0;
+}
+
+unsigned int Controller::rtt_avg() {
+    uint64_t total = 0;
+    for (int i = 0; i < NUM_RTTS; i++) {
+        total += rtts[i];
+    }
+    unsigned int avg = total/NUM_RTTS;
+    return avg;
+}
+
 /* A datagram was sent */
 void Controller::datagram_was_sent( const uint64_t sequence_number,
 				    /* of the sent datagram */
@@ -37,6 +74,7 @@ void Controller::datagram_was_sent( const uint64_t sequence_number,
                                     /* in milliseconds */
 {
   /* Default: take no action */
+  update(send_timestamp);
 
   if ( debug_ ) {
     cerr << "At time " << send_timestamp
@@ -55,21 +93,14 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
                                /* when the ack was received (by sender) */
 {
   /* Default: take no action */
-  if (cwnd < slow_st_thresh) {
-    cwnd += 1;
-  } else {
-    cwnd += 1/cwnd;
-  }
   unsigned int curr_rtt = (timestamp_ack_received - recv_timestamp_acked) +
       (recv_timestamp_acked - send_timestamp_acked);
-  float old_avg = rtt_avg;
-  float curr_weight = 1/cwnd;
-  rtt_avg = curr_weight * curr_rtt + (1 - curr_weight) * rtt_avg;
-  if (rtt_avg > old_avg && old_avg < RTT_THRESH) {
-    if (rtt_avg > RTT_THRESH) {
-        cwnd /= 2; 
-    }
-  }
+  rtt_add(curr_rtt);
+  update(timestamp_ack_received);
+  acks_received++;
+
+  cwnd += 1/cwnd;
+
 
   if ( debug_ ) {
     cerr << "At time " << timestamp_ack_received
--- datagrump/controller.hh
+++ datagrump/controller.hh
@@ -3,15 +3,26 @@
 
 #include <cstdint>
 
+#define NUM_RTTS 10
+
 /* Congestion controller interface */
 
 class Controller
 {
 private:
   bool debug_; /* Enables debugging output */
-  float cwnd;
-  float rtt_avg;
-  float slow_st_thresh;
+  double cwnd;
+  double slow_st_thresh;
+
+  int acks_received;
+  unsigned int rtts[NUM_RTTS];
+  int index;
+  uint64_t time_slice_start;
+  double est_throughput;
+
+  void update(uint64_t cur_time);
+  void rtt_add(unsigned int rtt);
+  unsigned int rtt_avg();
 
 
   /* Add member variables here */
