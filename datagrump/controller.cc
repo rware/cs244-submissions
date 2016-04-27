3160d850a4db0eae39960465e3b2d033d76b3076
Varun Ramesh <varunramesh@live.com>
2016-04-27 04:15:58 -0700

    Score of 30

--- datagrump/controller.cc
+++ datagrump/controller.cc
@@ -3,6 +3,8 @@
 #include "controller.hh"
 #include "timestamp.hh"
 
+#include <limits>
+
 using namespace std;
 
 double curr_window_size = 5; //Slow start, so begin with 5
@@ -10,6 +12,7 @@ unsigned int multiplicative_factor = 2; // The factor by which we decrease our w
 unsigned int additive_factor = 1; // The factor by which we increase our window during a congestion event
 unsigned int prop_delay_threshold = 155; // one-way propogation time threshold, for congestion event detection
 
+unsigned int MIN_WINDOW_SIZE = 5;
 
 /* Default constructor */
 Controller::Controller( const bool debug )
@@ -21,7 +24,7 @@ unsigned int Controller::window_size( void )
 {
   /* Default: fixed window size of 100 outstanding datagrams */
   unsigned int the_window_size = (unsigned int) curr_window_size;
-  if (the_window_size < 1) the_window_size = 1;
+  if (the_window_size < MIN_WINDOW_SIZE) the_window_size = MIN_WINDOW_SIZE;
 
   if ( debug_ ) {
     cerr << "At time " << timestamp_ms() << " window size is " <<
@@ -56,15 +59,9 @@ void Controller::on_timeout( void )
   // }
 }
 
-uint64_t prev_rtt = 0;
-
-static uint64_t RTT_LOW = 0;
-static uint64_t RTT_HIGH = 200;
-static uint64_t MIN_RTT = 0;
-static double alpha = 1.0;
-static double beta = 1.0;
-static double rtt_diff = 0;
-static int negative_grad_counter = 0;
+double base_delay = std::numeric_limits<double>::infinity();
+double TARGET_DELAY = 20;
+double GAIN = .05;
 
 /* An ack was received */
 void Controller::ack_received( const uint64_t sequence_number_acked,
@@ -76,39 +73,12 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
 			       const uint64_t timestamp_ack_received )
                                /* when the ack was received (by sender) */
 {
-  uint64_t new_rtt = timestamp_ack_received - send_timestamp_acked;
-  if (MIN_RTT == 0) {
-    MIN_RTT = new_rtt;
-    prev_rtt = new_rtt;
-    cout << "Min RTT: " << MIN_RTT << endl;
-    return;
-  }
+  double delay = (double)recv_timestamp_acked - (double)send_timestamp_acked;
+  base_delay = std::min(base_delay, delay);
+  double queueing_delay = delay - base_delay;
 
-  double new_rtt_diff = (double)new_rtt - (double)prev_rtt;
-  prev_rtt = new_rtt;
-  rtt_diff = (1 - alpha)*rtt_diff + alpha*new_rtt_diff;
-  double normalized_gradient = rtt_diff / MIN_RTT;
-  if (new_rtt < RTT_LOW) {
-    /* Additive increase of the window size. */
-    curr_window_size += additive_factor / curr_window_size;
-  } else if (new_rtt > RTT_HIGH) {
-    /* Multiplicative decrease. */
-    cout << "Mutliplicative decrease." << endl;
-    curr_window_size /= multiplicative_factor;
-  } else if (normalized_gradient <= 0) {
-    if (negative_grad_counter > 5) {
-      curr_window_size += 5 * additive_factor / curr_window_size;
-    } else {
-      curr_window_size += additive_factor / curr_window_size;
-    }
-
-    negative_grad_counter++;
-  } else {
-    //cout << normalized_gradient << endl;
-    negative_grad_counter = 0;
-    curr_window_size = curr_window_size - beta*normalized_gradient;
-  }
-
+  double off_target = TARGET_DELAY - queueing_delay;
+  curr_window_size += GAIN * off_target / curr_window_size;
   if (curr_window_size < 1) curr_window_size = 1;
 
   if ( debug_ ) {
