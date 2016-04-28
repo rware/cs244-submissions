94db8621a62217f33a85cd0fd91f99cf53481c35
Varun Ramesh <varunramesh@live.com>
2016-04-28 01:10:03 -0700

    Better graphing

--- datagrump/view_graph.py
+++ datagrump/view_graph.py
@@ -2,6 +2,8 @@ import time
 import subprocess
 import select
 import sys
+from Queue import Queue
+from threading import Thread
 
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -10,34 +12,39 @@ import numpy as np
 filename = sys.argv[1]
 print filename
 
-f = subprocess.Popen(['tail', '-n', '1', '-F',filename],
-    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-p = select.poll()
-p.register(f.stdout)
+
 
 start_time = time.time()
 times, values = [], []
-TIME_WINDOW = 10
-DELETE_WINDOW = 20
+TIME_WINDOW = 5
 
 AXIS_FACTOR = 0.25
 
-def animate(frameno):
-    while p.poll(0):
+data_queue = Queue()
+def tail():
+    f = subprocess.Popen(['tail', '-n', '1', '-F',filename],
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    while True:
         value = float(f.stdout.readline())
         current_time = time.time() - start_time
+        data_queue.put((current_time, value))
+Thread(target=tail).start()
 
+def animate(frameno):
+    while not data_queue.empty():
+        (current_time, value) = data_queue.get()
         times.append(current_time)
         values.append(value)
 
-        line.set_data(times, values)
-        max_val, min_val = max(values), min(values)
-        ax.set_ylim(min_val - abs(min_val)*AXIS_FACTOR, max_val + abs(max_val)*AXIS_FACTOR)
-        ax.set_xlim(current_time - TIME_WINDOW, current_time)
+    current_time = time.time() - start_time
+    while len(times) > 0 and times[0] < current_time - TIME_WINDOW:
+        times.pop(0)
+        values.pop(0)
 
-        while len(times) > 0 and times[0] < current_time - DELETE_WINDOW:
-            times.pop(0)
-            values.pop(0)
+    line.set_data(times, values)
+    max_val, min_val = max(values), min(values)
+    ax.set_ylim(min_val - abs(min_val)*AXIS_FACTOR, max_val + abs(max_val)*AXIS_FACTOR)
+    ax.set_xlim(current_time - TIME_WINDOW, current_time)
 
 fig, ax = plt.subplots()
 plt.title(filename)
